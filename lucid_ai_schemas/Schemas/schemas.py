245dc7c48dcb3da2e4d2f2a055a46b08d053ab96
Dan <dan.lesser@ogcglobal.com>
2025-03-18 18:52:46 +0200

    feat: added two more schemas that were missing

--- lucid_ai_schemas/Schemas/schemas.py
+++ lucid_ai_schemas/Schemas/schemas.py
@@ -20,6 +20,25 @@ class PromptUpdateSchema(Ai_utilsBase):
     engine: Optional[str] = None
 
 
+class OrchestratorSchema(Ai_utilsBase):
+    branch_id: Optional[int] = Field(
+        default=None,
+        description="The branch ID of the business.")
+    freetext: Optional[str] = Field(
+        default=None,
+        description="""A conversational description of the business
+        by the client."""
+    )
+
+
+class LogAIResponseObject(Ai_utilsBase):
+    prompt_object: Optional[dict] = None
+    response: Optional[dict] = None
+    response_time: Optional[float] = None
+    payload: Optional[dict] = None
+    parsed_input: Optional[str] = None
+
+
 # General Prompts
 class GraphType(Enum):
     Area = "Area"
